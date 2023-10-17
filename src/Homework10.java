a4d57591484042cbed60843337cf358b4f400d64
Nikasan1 <nikasan4@gmail.com>
2023-10-17 03:08:25 +0200

    Text changed,and shuttle's number starts from 1

--- src/Homework10.java
+++ src/Homework10.java
@@ -2,16 +2,16 @@ public class Homework10 {
     public static void main(String[] args) {
         int[] a = new int[100];
         int index = 0;
-        int number = 0;
+        int number = 1;
         for (; index < 100; index++) {
             a[index] = number;
             if ((number < 40 & number % 5 == 4) | (number > 50) & (number < 90) & number % 5 == 4 |(number < 140 & number % 5 == 4) | (number > 150) & (number < 190) & number % 5 == 4) {
                 index--;
-            } else if (((number >= 40) & (number < 50)) | ((number >= 90) & (number < 100))|((number >= 140) & (number < 150)) | ((number >= 190) & (number < 200))) {
+            } else if (((number >= 40) & (number < 50)) | ((number >= 90) & (number < 100))|((number >= 140) & (number < 150))) {
                 index--;
             } else {
-                System.out.print("index " + index + " ");
-                System.out.println("number " + a[index]);
+                System.out.print("Порядковий номер шатлу: " + index + " ");
+                System.out.println("Число шатлу: " + a[index]);
             }
             number++;
         }
